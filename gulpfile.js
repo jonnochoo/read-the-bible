c32b657c2bb526868763c81142dead4de795923c
Jonathon Choo <jonno.choo@gmail.com>
2014-12-30 22:03:34 +1100

    Migrated to using SASS for CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,24 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
-var watch = require('gulp-watch');
+var minifycss = require('gulp-minify-css');
+var minifycss = require('gulp-minify-css');
 var nodemon = require('gulp-nodemon');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
 
 gulp.task('default', function() {
-  
+  gulp.watch("public/scss/**/*.scss", ['sass']);  
+});
+
+gulp.task('sass', function () {
+  return gulp.src('public/scss/app.scss')
+    .pipe(sass({ 
+      errLogToConsole: true,
+      includePaths: ['./bower_components/foundation/scss']
+    }))
+    .pipe(gulp.dest('public/css/'))
+    .pipe(minifycss())
+    .pipe(gulp.dest('public/css/'));
 });
 
 gulp.task('develop', function () {
