f6b6ec751fd8cd4a718e88e0c7b9c26f14282394
Jonathon Choo <jonno.choo@gmail.com>
2015-01-14 22:43:42 +1100

    Added functionality to browse next and previous chapters by using arrow keys

--- public/js/verses.js
+++ public/js/verses.js
@@ -9,9 +9,9 @@ listener.simple_combo("/", function() {
   $(document).foundation('reveal', { animation: 'fade' });
   $('#searchControl').foundation('reveal', 'open');        
 });
-listener.simple_combo("right", function() {
-  window.location ="/";
-});
 listener.simple_combo("left", function() {
-  window.location ="/";
+  window.location = $("#previousChapter").attr('href');
+});
+listener.simple_combo("right", function() {
+  window.location = $("#nextChapter").attr('href');
 });
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var request = require('request');
 var express = require('express');
+var verseQueryParser = require('../lib/verseQueryParser');
 var chapterFinder = require('../lib/chapterFinder');
+
 var router = express.Router();
 
 router.get('/', function (req, res) {
@@ -8,19 +10,25 @@ router.get('/', function (req, res) {
 });
 
 router.post('/', function (req, res) {  
-  var uri = '/' + req.body.search.trim().replace(' ', '+');
+  var info = verseQueryParser.parse(req.body.search.trim());
+  var uri = '/' + info.book.replace(' ', '+') + '+' + info.chapter + (info.verse ? (':' + info.verse) : '' );  
   res.redirect(uri);
 });
 
 router.get('/:verse', function (req, res) {
-
   var verse = req.params.verse;
+  var verseInfo = verseQueryParser.parse(verse);
+  var chapter = parseInt(verseInfo.chapter);
+  var nextChapter = chapterFinder.getNextChapter(verseInfo.book, chapter);
+  var nextChapterUri = nextChapter.book.replace(' ', '+') + '+' + nextChapter.chapter;
+  var previousChapter = chapterFinder.getPreviousChapter(verseInfo.book, chapter);
+  var previousChapterUri = previousChapter.book.replace(' ', '+') + '+' + previousChapter.chapter;
+
   var url = 'http://www.esvapi.org/v2/rest/passageQuery?key=IP&include-audio-link=0&include-first-verse-numbers=0&include-verse-numbers=0&include-headings=0&include-footnotes=0&include-short-copyright=0&passage=' + verse;
 
   request.get(url, function(err, response, data){
-    res.render('verses', { html: data });
+    res.render('verses', { html: data, nextChapterUri: nextChapterUri, previousChapterUri: previousChapterUri });
   });
-    
 });
 
 module.exports = router;
